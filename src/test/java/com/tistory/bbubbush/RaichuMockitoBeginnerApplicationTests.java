8b3fbdc9a8d666cdbadc4e2a4d0753e910b0cfb6
bbubbush <bbubbush@gmail.com>
2020-11-03 15:10:22 +0900

    git setting

---
+++ src/test/java/com/tistory/bbubbush/RaichuMockitoBeginnerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tistory.bbubbush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class RaichuMockitoBeginnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello world!");
+    }
+
+}
